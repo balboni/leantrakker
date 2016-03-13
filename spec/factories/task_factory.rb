d5d3dcacefa4b4f62d1729127f994266d82f22f0
balboni <brian@brianbalboni.com>
2016-03-13 11:31:22 -0500

    added task view to show.  all green p139/5.2.1 complete

---
+++ spec/factories/task_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do  
+	factory :task do  
+		subject "Example task" 
+		description "An example task, nothing more"
+	end
+end
---
+++ spec/features/viewing_tasks_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.feature "Users can view tickets" do  
+	before do  
+		sublime = FactoryGirl.create(:project, name: "Sublime Text 3")
+		FactoryGirl.create(:task, project: sublime,
+			subject: "Make it shiny!",
+			description: "Gradients! Starbursts! Oh my!")
+
+		ie = FactoryGirl.create(:project, name: "Internet Explorer")
+		FactoryGirl.create(:task, project: ie,
+			subject: "Standards compliance", description: "Isn't a joke.")
+
+		visit "/"
+	end
+
+	scenario "for a given project" do  
+		click_link "Sublime Text 3"
+
+		expect(page).to have_content "Make it shiny!"
+		expect(page).to_not have_content "Standards compliance"
+
+		click_link "Make it shiny!"
+		within("#task h2") do  
+			expect(page).to have_content "Make it shiny!"
+		end
+
+		expect(page).to have_content "Gradients! Starbursts! Oh my!"
+	end
+end
+
+
